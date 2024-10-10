87f3b263586cdc8ef4fb98bbf974e74cc82cb79a
Zaide1 <zaideebrahim@gmail.com>
2024-10-10 17:22:23 +0100

    Initial comit

---
+++ mydb.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'password'
+
+
+)
+
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("Created")
+print(mysql.connector.__version__)
